9346b2082bcf49a6bbfb1f4df6e543d0c320a8c9
Aswinikumar555 <37668545+Aswinikumar555@users.noreply.github.com>
2020-02-11 17:51:56 +0530

    Update BooksAPI.js

--- src/BooksAPI.js
+++ src/BooksAPI.js
@@ -1,4 +1,16 @@
 import axios from 'axios'
+
+//promise
+export const getAll = () => {
+return new Promise((resolve, reject)=> {
+fetch('http://172.16.18.240:8081/testapp/user/getUsers', { mode: 'no-cors' })
+.then(res => ()=>{let a=res.json();resolve(a)})
+.catch(data => reject(data))
+})
+}
+
+
+
 export const getAll = () =>
   fetch('/testapp/book/getBooks')
     .then(res => res.json())
